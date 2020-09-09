3dc630caa68221422c099b73c5b518dafa85568a
Max Shevchencko <tier4ik@gmail.com>
2020-09-09 14:27:42 +0400

    Added gridsome

---
+++ e-vents-frontend/src/layouts/Default.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>E-Vents</v-toolbar-title>
+      <v-text-field
+        placeholder="Search"
+        style="max-width: 400px;"
+        prepend-inner-icon="mdi-magnify"
+        class="ml-8"
+        outlined
+        rounded
+        dense
+        hide-details />
+      <v-spacer />
+      <v-btn>Create an Event</v-btn>
+    </v-app-bar>
+    <v-main>
+      <slot/>
+    </v-main>
+  </v-app>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
